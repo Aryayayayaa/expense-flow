0a897265f58400be3c245099ee99ea3a5772383d
Arya Jain <arya.jain@geekyants.com>
2026-07-30 23:38:47 +0530

    feat: add dynamic expense details route

--- src/app/expenses/[id]/page.tsx
+++ src/app/expenses/[id]/page.tsx
@@ -1,3 +1,17 @@
-export default function IDPage() {
-  return <h1>ID Page</h1>;
+type ExpensePageProps = {
+  params: Promise<{
+    id: string;
+  }>;
+};
+
+export default async function ExpensePage({ params }: ExpensePageProps) {
+  const { id } = await params;
+
+  return (
+    <main>
+      <h1>Expense Details</h1>
+
+      <p>Expense ID: {id}</p>
+    </main>
+  );
 }
